b936d265f562cf570ecb8e4d0f416528976d78eb
RohanManoharan <rohan.manoharan@ryerson.ca>
2024-12-03 22:01:26 -0500

    deploy

--- docs/server/angular-app-manifest.mjs
+++ docs/server/angular-app-manifest.mjs
@@ -9,9 +9,9 @@ export default {
   }
 ],
   assets: new Map([
-['index.csr.html', {size: 457, hash: '1aca2c5a458a90284edc1f473eb8d20619abd2b4fd2f5a538c0c28c67a4f85fa', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)}], 
-['index.server.html', {size: 970, hash: '17d9e8f21e14bb730ebe2ac48fa02dba3b0c7c3a3876b5362f32e3b9170f7673', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)}], 
-['index.html', {size: 3665, hash: 'd47d6b5df2e88cbf5416e71ab652fa3bbb20fe9de93fab4ffc488da00849203e', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)}], 
+['index.csr.html', {size: 457, hash: 'bdbac58a0e6b2c22d3f1ef8bc1a453286436ee280c33efe614f9de9155cba115', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)}], 
+['index.server.html', {size: 970, hash: '75a78c7ce6ee4ce1d5efff6f87e2db6bbc5bba9eda3b59a30de9aa69a9e120cc', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)}], 
+['index.html', {size: 4923, hash: 'e0a8e51f1cc2aa259b796269b2769add1db233116e1225e62c5381d6175b16a5', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)}], 
 ['styles-5INURTSO.css', {size: 0, hash: 'menYUTfbRu8', text: () => import('./assets-chunks/styles-5INURTSO_css.mjs').then(m => m.default)}]
 ]),
   locale: undefined,
--- docs/server/assets-chunks/index_csr_html.mjs
+++ docs/server/assets-chunks/index_csr_html.mjs
@@ -8,6 +8,6 @@ export default `<!doctype html>
 <link rel="stylesheet" href="styles-5INURTSO.css"></head>
 <body ngcm="">
   <app-root></app-root>
-<script src="polyfills-FFHMD2TL.js" type="module"></script><script src="main-HUBWTL4M.js" type="module"></script></body>
+<script src="polyfills-FFHMD2TL.js" type="module"></script><script src="main-LXGLX7YS.js" type="module"></script></body>
 </html>
 `;
